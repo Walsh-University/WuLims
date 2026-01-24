72333b31cfbc793393adaea0d5f3124b694cc929
David Good <dgood@programminggoody.com>
2026-01-24 12:01:13 -0500

    Fixes for running in Docker and k3s

---
+++ config/settings_test.py
@@ -0,0 +1,15 @@
+from .settings import *  # noqa
+
+# Force SQLite for tests (no external services needed)
+DATABASES = {
+        "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": BASE_DIR / "test_db.sqlite3",
+        }
+}
+
+# Make tests a little faster / quieter
+PASSWORD_HASHERS = [
+        "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
