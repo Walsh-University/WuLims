204b2a75c1f9461723e36067ec4b2f8b712ae107
olesyaivashchuk24 <olesa200524@gmail.com>
2026-01-29 11:21:37 -0500

    Fix

--- instruments/views.py
+++ instruments/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
+
 from django.http import HttpResponse, HttpResponseBadRequest
+
 from django.shortcuts import get_object_or_404, render
+
 from django.template.loader import render_to_string
 
 from .forms import InstrumentFilterForm
