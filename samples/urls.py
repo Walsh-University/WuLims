12c706b9153136a5449826ccc5651c7d9a936489
David Good <787309+davidgood@users.noreply.github.com>
2026-01-29 19:04:09 -0500

    Samples Refactor (#32)
    * Add Django Relationships Doc
    
    * fix htmx, alpineJS, and add pretty samples highlight
    
    * Add sample_add method stub
    
    * Refactor sample_id to be a UUID. Refactor Sample Add to be non-admin.
    
    * untrack .idea folder

--- samples/urls.py
+++ samples/urls.py
@@ -6,8 +6,9 @@ app_name = "samples"
 
 urlpatterns = [
     path("", views.sample_list, name="list"),
+    path("add/", views.sample_add, name="add"),
     path("_table/", views.sample_table, name="table"),
-    path("<int:pk>/", views.sample_detail, name="detail"),
-    path("<int:pk>/_approve_modal/", views.approve_modal, name="approve_modal"),
-    path("<int:pk>/approve/", views.approve_sample, name="approve"),
+    path("<uuid:pk>/", views.sample_detail, name="detail"),
+    path("<uuid:pk>/_approve_modal/", views.approve_modal, name="approve_modal"),
+    path("<uuid:pk>/approve/", views.approve_sample, name="approve"),
 ]
