b35d715f835af8dc62d94f5106ff55c6c50730a9
logantrent <ltrent1@walsh.edu>
2026-02-03 14:10:03 -0500

    Update urls.py

--- lims_core/urls.py
+++ lims_core/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from lims_core import views
 
 app_name = "lims_core"
@@ -7,4 +6,7 @@ app_name = "lims_core"
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("search/", views.search, name="search"),
+
+    path("api/status", views.status_public, name="status_public"),
+    path("api/status/internal", views.status_internal, name="status_internal"),
 ]
