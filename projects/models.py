1f64670e8301d704f1ad2e679ccb66884dbdf283
pmccord2003 <pmccord2003@gmail.com>
2026-02-04 11:00:55 -0500

    Payton/projects (#43)
    * create projects app
    
    * add projects app to base.py
    
    * add model for projects
    
    * ruff fixes
    
    * remove customers foreign key for the time being
    
    * add sample project to conftest.py
    
    * add tests for project model
    
    * add migration for project model
    
    * Merged main and ran ruff fix for Payton.
    
    ---------
    
    Co-authored-by: David Good <787309+davidgood@users.noreply.github.com>
    Co-authored-by: David Good <dgood@programminggoody.com>

---
+++ projects/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Project(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = "ACTIVE"
+        CLOSED = "CLOSED"
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.ACTIVE)
+    start_date = models.DateField()
+    completed_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
