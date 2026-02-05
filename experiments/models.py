dd765670d24d23b2d2fdf549b7027fc39c96d219
pmccord2003 <pmccord2003@gmail.com>
2026-02-04 21:56:19 -0500

    Payton/experiments (#48)
    * add experiments to installed_apps
    
    * Create experiments app and model
    
    * ruff fixes
    
    * ruff format fix
    
    * add Project foreign key to experiments model
    
    * create experiments migration
    
    * add tests for experiments model

---
+++ experiments/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from projects.models import Project
+
+
+class Experiment(models.Model):
+    class Status(models.TextChoices):
+        CREATED = "CREATED"
+        RUNNING = "RUNNING"
+        COMPLETED = "COMPLETED"
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=30, choices=Status.choices, default=Status.CREATED)
+    created_at = models.DateTimeField(auto_now_add=True)
+    data_file = models.CharField(max_length=255)
+    version = models.CharField(max_length=20)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
