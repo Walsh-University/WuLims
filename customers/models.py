3411a11174ea3651663e1d65f8628ec1ff636f2e
Joey <jatimco7@gmail.com>
2026-02-06 16:41:01 -0500

    customer model added (#27)
    * customer model added
    
    * new changes to customers
    
    * ruff check
    
    * added UUID
    
    * changed migrations for UUID
    
    * removed settings.py

---
+++ customers/models.py
@@ -0,0 +1,20 @@
+import uuid
+
+from django.db import models
+
+
+class Customer(models.Model):
+    class Active(models.TextChoices):
+        ACTIVE = "ACTIVE"
+        INACTIVE = "INACTIVE"
+
+    customer_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer_name = models.CharField(max_length=200)
+    external_id = models.CharField(max_length=32, unique=True)
+    customer_type = models.CharField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.CharField(max_length=20, choices=Active.choices, default=Active.INACTIVE)
+
+    def __str__(self):
+        return self.customer_id
