f687df1f8aef8152f8eb03b5c009cb1e075bfd97
David Good <dgood@programminggoody.com>
2026-02-13 15:19:44 -0500

    Update unit test

--- tests/test_results.py
+++ tests/test_results.py
@@ -219,7 +219,7 @@ class TestResultTableView:
         assert_that(response.status_code).is_equal_to(200)
         assert_that(content).contains(str(result.id))
         assert_that(content).contains(project.name)
-        assert_that(content).contains(str(sample.sample_id))
+        assert_that(content).contains(str(sample.sample_name))
 
     def test_table_filters_by_status(self, authenticated_client, sample, project):
         acquired = Result.objects.create(
