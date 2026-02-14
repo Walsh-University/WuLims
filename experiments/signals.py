02522944de629a41a2b0fbc2107dda76f8c082c8
olesyaivashchuk24 <olesa200524@gmail.com>
2026-02-14 11:49:20 -0500

    Maybe fix

--- experiments/signals.py
+++ experiments/signals.py
@@ -1,27 +1,19 @@
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
+from .models import Experiment
 from audit.services import log_audit_event
 
-from .models import Experiment
+
+def get_current_user():
+    return None
 
 
 @receiver(post_save, sender=Experiment)
 def audit_experiment_save(sender, instance, created, **kwargs):
     action = "create" if created else "update"
-    log_audit_event(
-        user=None,
-        action=action,
-        instance=instance,
-        diff=None,
-    )
+    log_audit_event(user=get_current_user(), action=action, instance=instance, diff=None)
 
 
 @receiver(post_delete, sender=Experiment)
 def audit_experiment_delete(sender, instance, **kwargs):
-    log_audit_event(
-        user=None,
-        action="delete",
-        instance=instance,
-        diff=None,
-    )
+    log_audit_event(user=get_current_user(), action="delete", instance=instance, diff=None)
