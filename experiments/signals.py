303ea6d83bcaf0335c924356b6f43df5907085d3
olesyaivashchuk24 <olesa200524@gmail.com>
2026-02-14 12:08:53 -0500

    Fix

--- experiments/signals.py
+++ experiments/signals.py
@@ -1,8 +1,9 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from .models import Experiment
 from audit.services import log_audit_event
+
+from .models import Experiment
 
 
 @receiver(post_save, sender=Experiment)
