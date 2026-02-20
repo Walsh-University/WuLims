93af00caee604e95b38f9715dbe2a2a28ad7b886
David Good <dgood@programminggoody.com>
2026-02-20 15:06:15 -0500

    update unit tests

--- tests/instruments/test_instruments.py
+++ tests/instruments/test_instruments.py
@@ -74,3 +74,37 @@ class TestInstrumentViews:
 
         content = response.content.decode()
         assert f"instrument-row-{instrument.pk}" in content
+
+    def test_toggle_active_rejects_get(self, authenticated_client, instrument):
+        url = reverse("instruments:toggle_active", args=[instrument.pk])
+        response = authenticated_client.get(url)
+        assert response.status_code == 400
+
+    def test_table_filters_by_name_query(self, authenticated_client, instrument):
+        url = reverse("instruments:table")
+        response = authenticated_client.get(url, {"q": instrument.name})
+        assert response.status_code == 200
+        assert instrument.name.encode() in response.content
+
+    def test_table_excludes_non_matching_query(self, authenticated_client, instrument):
+        url = reverse("instruments:table")
+        response = authenticated_client.get(url, {"q": "zzz-no-match"})
+        assert response.status_code == 200
+        assert instrument.name.encode() not in response.content
+
+    def test_table_filters_by_manufacturer(self, authenticated_client, instrument):
+        url = reverse("instruments:table")
+        response = authenticated_client.get(url, {"manufacturer": instrument.manufacturer})
+        assert response.status_code == 200
+        assert instrument.name.encode() in response.content
+
+    def test_detail_tab_overview(self, authenticated_client, instrument):
+        url = reverse("instruments:detail", args=[instrument.pk])
+        response = authenticated_client.get(url, {"tab": "overview"})
+        assert response.status_code == 200
+        assert instrument.manufacturer.encode() in response.content
+
+    def test_detail_tab_maintenance(self, authenticated_client, instrument):
+        url = reverse("instruments:detail", args=[instrument.pk])
+        response = authenticated_client.get(url, {"tab": "maintenance"})
+        assert response.status_code == 200
